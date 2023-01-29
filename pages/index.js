e38c162388f2a6d4345ae680a525b57cd2d73d9a
chidiebereojingwa <ojingwaoji@gmail.com>
2023-01-29 03:19:41 +0100

    converted to class component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
+import React from 'react';
 import styles from './index.module.css';
 import Card from './Card'
+import data from './API/data.json'
 
-export default () => {
-    return (
+export default class Index extends React.Component {
+
+    render() { 
+        return (
         <div className={styles.card}>
             <header className={styles.header}>
                 <img src='/logo.png' className={styles.logo} alt='logo' />
@@ -14,4 +18,5 @@ export default () => {
             </div>
         </div>
     )
+    }    
 }
