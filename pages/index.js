653db7b6b3e6101cd8e5aaf82e7e4e0618a7b1a8
chidiebereojingwa <ojingwaoji@gmail.com>
2023-01-29 03:08:48 +0100

    updated index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,12 @@ import Card from './Card'
 export default () => {
     return (
         <div className={styles.card}>
-            <header className={styles.header}></header>
+            <header className={styles.header}>
+                <image src='/logo.png' className={styles.logo} alt='logo' />
+            </header>
+            <div className={styles.grid}>
+                
+            </div>
         </div>
     )
 }
