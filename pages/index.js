1b283470a72a4aace3464cf2dd3263db1a173916
chidiebereojingwa <ojingwaoji@gmail.com>
2023-01-29 03:06:13 +0100

    imported modules

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,10 @@
-export default () => (
-    <p1>Hello we  are good</p1>
-)
+import styles from './index.module.css';
+import Card from './Card'
+
+export default () => {
+    return (
+        <div className={styles.card}>
+            <header className={styles.header}></header>
+        </div>
+    )
+}
